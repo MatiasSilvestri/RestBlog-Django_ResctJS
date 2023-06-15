31556dc8c3b58047ef1417d35d9f9cca51d7908e
MatiasS.dev() <94636694+MatiasSilvestri@users.noreply.github.com>
2023-06-15 15:09:09 -0300

    main

--- CRUD-Django-ReactJS-MySQL/src/components/Navbar/Navbar.js
+++ CRUD-Django-ReactJS-MySQL/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          UskoKruM2010 | React CRUD | Django REST API
+          MattSDev - Back-End Developer
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
